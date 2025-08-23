8e5e0b74129398d3f462dd9e22a6c27eef1ae615
Arise Damilare <arisedamilare5@gmail.com>
2025-08-23 14:11:23 +0100

    python class week 2

---
+++ index.py
@@ -0,0 +1,98 @@
+# print("Hello World.")
+
+# Core concepts about 
+# 1. It is a high level pragramming language
+# 2. it is versatile
+# 3. it is open source
+# 4. it is object oriented
+
+# Commenting
+# 1. For documentation
+# 2. For debugging
+
+# block comment
+# line comment
+"""
+    I am a student of SQI in the department 
+    of AI
+"""
+
+# print("""
+      
+#     Welcome to class Damilare
+#     1. sign in
+#     2. sign up
+      
+#     """)
+
+
+# Indentation
+# print("Hello")
+
+# def run():
+#     print('I am running')
+
+# run()
+
+
+
+# python varaibles
+# variables are symbolic names that acts as containers for storing values
+# i. variable name
+# ii. assignment operation (funnel)
+# iii. value
+box = "cloths"
+box = "Water"
+# print(box)
+
+# Rules guiding python variable declarations
+# 1. You don't start a variable name with any symbols aside _ or alphabet
+# 2. You don't include spaces during variable declaration. You use either camel casing, pascal casing or snake casing 
+# 3. You don't use python's special keywords
+# 4. your varaible name should be descriptive enough 
+
+
+first_name = 'Damilare' # snake casing
+FirstName = 'Damilare' # pascal casing
+firstName = 'Damilare' # Camel casing
+
+# types of variable declaration
+# 1. single variable single value
+first_name = 'Damilare'
+
+# 2. single variable multiple values
+students = 'raji', 'precious', 'emmanuel'
+# print(students)
+
+# 3. multiple variable single value 
+x = y = z = 10
+# print(z)
+# z = z + 2
+# print(x)
+
+# 4. multiple variables multiple values
+# x, y, z = 10, 11, 12
+# print(z)
+
+
+# Dynamic variable
+name = input('Your name: ')
+age = input('Age: ')
+dept = input("Department: ")
+# print(name)
+
+# Concatenation -> joining two strings together
+# print("I am " + "Happy " + "100")
+
+# print("Welcome",100)
+# print("My name is "+name+" I am "+age+"Years old. I am in the department of "+dept+". Thank you")
+
+# f-string 
+print(f"My name {name}, I am {age}years old. I am in the department of {dept}. Thank you.")
+
+# python datatypes
+# python operators
+# conditional statement
+
+# Build a simple alata calculator
+
